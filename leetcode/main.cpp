bdfaec769c3b390dda6999131541b50714c389a4
cheneyshen <shenfei2031@hotmail.com>
2016-08-25 23:08:16 -0500

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by spring  on 8/25/16.
+//  Copyright © 2016 spring . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
