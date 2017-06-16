0c1e273d555249444795545d895496084947483f
cheneyshen <shenfei2031@hotmail.com>
2017-06-16 02:55:34 -0500

    Solution2-Solution100

---
+++ laicode_java/src/laicode_java/Solution74.java
@@ -0,0 +1,23 @@
+package laicode_java;
+
+public class Solution74 {
+	//复制java文件
+	//for i in {3..100}; do cp Solution2.java "Solution$i.java"; done
+	//分离某一行然后替换
+	//for i in {3..100}; do head -3 "Solution$i.java" | tail -1 $1 | awk '{split($0, a, ' '); print a[3]' ; done
+	//sed -i ' ' 's/hi/hello/' file1
+	//更改后复制到新文件
+	//awk 'NR==2{gsub("hi", "Hello", $1)}; {print $0}' file1 > newfile
+	//代变量就必须要用“”双引号
+	//for i in {3..100}; do sed -i " " "3s/Solution/Solution$i/" Solution$i.java ; done
+	public void printit() {
+		System.out.println("aa");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Solution1 s1 = new Solution1();
+		s1.printit();
+		return;
+	}
+}
