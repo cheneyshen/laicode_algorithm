39012271c1525f44618a092e3afde17ace663d11
fshen4 <fshen4@SB112EH21.otsads.iit.edu>
2017-06-26 15:07:33 -0500

    135

--- laicode_java/src/laicode_java/Solution135.java
+++ laicode_java/src/laicode_java/Solution135.java
@@ -3,8 +3,19 @@ package laicode_java;
 //Merge Sort Linked List
 public class Solution135 {
 	public ListNode mergeSort(ListNode head) {
+		if(head==null || head.next==null) {
+			return head;
+		}
 		ListNode left=head;
 		ListNode right=getMiddle(head);
+		ListNode prev=right;
+		right=right.next;
+		if(prev!=null) {
+			prev.next=null;
+		}
+		left=mergeSort(left);
+		right=mergeSort(right);
+		head=merge(left, right);
 		return head;
 	}
 //	4 2 6 -3 5
@@ -51,23 +62,24 @@ public class Solution135 {
 				if(left.value<=right.value) {
 					index.next=left;
 					left=left.next;
+					index=index.next;
 				}
 				else {
 					index.next=right;
 					right=right.next;
+					index=index.next;
 				}
 			}
-			while(left!=null) {
-				index.next=left;
-				left=left.next;
+			if(left!=null) {
+				index.next=left;				
 			}
-			while(right!=null) {
+			if(right!=null) {
 				index.next=right;
-				right=right.next;
 			}
 			return result;
 		}
 	}
+	
 	public static void main(String[] args) {
 		Solution135 s135 = new Solution135();
 		ListNode l1=new ListNode(6);
@@ -77,11 +89,18 @@ public class Solution135 {
 		ListNode l5=new ListNode(4);
 		ListNode l6=new ListNode(3);
 		l1.next=l2;
-//		l2.next=l3;
-//		l3.next=l4;
-//		l4.next=l5;
-//		l5.next=l6;
-		ListNode result=s135.getMiddle(l1);
-		System.out.println(result.value);
+		l2.next=l3;
+		l3.next=l4;
+		l4.next=l5;
+		l5.next=l6;
+		ListNode result=s135.mergeSort(l1);
+		printLinkedList(result);
+	}
+	
+	public static void printLinkedList(ListNode head) {
+		for(ListNode node=head;node!=null;node=node.next) {
+			System.out.print(String.valueOf(node.value)+" ");
+		}
+		System.out.println("");
 	}
 }
