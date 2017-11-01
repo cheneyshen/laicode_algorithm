0f06ebf92fa7c64dd9dfac2514e2240790a0ad75
Fei Shen <shenfei2031@gmail.com>
2017-11-01 00:23:39 -0700

    305-306

--- laicode_java/src/laicode_java/Solution305.java
+++ laicode_java/src/laicode_java/Solution305.java
@@ -11,7 +11,64 @@ import java.util.*;
 //Input List:    1 -> 2 -> 3 -> 4 -> 5
 //Output List: 1 -> 3 -> 5 -> 4 -> 2
 public class Solution305 {
-
+/*
+ * KAltReverse(ListNode head, int k)
+ * 1) Reverse first k nodes
+ * 2) In the modified list head points to the kth node. So change next
+ * of head to (k+1)th node
+ * 3) Move the current pointer to skip next k nodes
+ * 4) Call the kAltReverse() recursively for rest of the n-2k nodes
+ * 5) Return new head of the list
+ */
+	static class ListNode {
+		int data;
+		ListNode next;
+		ListNode(int d) {
+			data = d;
+			next = null;
+		}
+	}
+	/*
+	 * Reverse alternate k nodes and
+	 * returns the pointer to the new head node
+	 */
+	ListNode kAltReverse(ListNode node, int k) {
+		ListNode curr = node;
+		ListNode next=null, prev=null;
+		int count=0;
+		//1. reverse first k nodes of the linked list
+		while(curr!=null && count<k) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+			count++;
+		}
+		
+		//2. Now head points to the kth node. 
+		// So change next of head to (k+1) node
+		if (node!=null) {
+			node.next = curr;
+		}
+		
+		//3. We do not want to reverse next k nodes. 
+		// So move the current pointer to skip next k nodes
+		count = 0;
+		while (count<k-1 && curr!=null) {
+			curr = curr.next;
+			count++;
+		}
+		
+		//4. Recursively call for the list starting from curr->next
+		// And make rest of the list as next of first node
+		if (curr!=null) {
+			curr.next = kAltReverse(curr.next, k);
+		}
+		
+		//5. prev is new head of the input list
+		return prev;
+	}
+	
 	public static void main(String[] args) {
 
 
--- laicode_java/src/laicode_java/Solution306.java
+++ laicode_java/src/laicode_java/Solution306.java
@@ -19,7 +19,80 @@ import java.util.*;
 //
 //Space complexity must be O(1)
 public class Solution306 {
-
+	static class ListNode {
+		char data;
+		ListNode next;
+		ListNode(char d) {
+			data = d;
+			next = null;
+		}
+	}
+	ListNode head;
+	ListNode slow, fast, second;
+	boolean isPalindrome(ListNode head) {
+		slow = head;
+		fast = head;
+		ListNode prev = null;
+		ListNode midd = null;
+		boolean result = true;
+		
+		if(head!=null && head.next!=null) {
+			while(fast!=null && fast.next!=null) {
+				fast = fast.next.next;
+				prev = slow;
+				slow = slow.next;
+			}
+			
+			if(fast!=null) {
+				midd = slow;
+				slow = slow.next;
+			}
+			
+			second = slow;
+			prev.next = null;
+			reverse();
+			result = compareHelper(head, second);
+			reverse();
+			if(midd!=null) {
+				prev.next = midd;
+				midd.next = second;
+			} else {
+				prev.next = second;
+			}
+		}
+		return result;
+	}
+	
+	void reverse() {
+		ListNode prev = null;
+		ListNode curr = second;
+		ListNode next;
+		while(curr!=null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		second = prev;
+	}
+	
+	boolean compareHelper(ListNode head1, ListNode head2) {
+		ListNode temp1 = head1;
+		ListNode temp2 = head2;
+		while (temp1 !=null && temp2 !=null) {
+			if(temp1.data == temp2.data) {
+				temp1 = temp1.next;
+				temp2 = temp2.next;
+			} else {
+				return false;
+			}
+		}
+		if(temp1==null && temp2==null) {
+			return true;
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 
 
