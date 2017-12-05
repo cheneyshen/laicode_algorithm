d5cb779fa93ebf5ea3bb098faf984290a58c99fe
Fei Shen <shenfei2031@gmail.com>
2017-12-05 00:18:43 -0800

    428-429

--- laicode_java/src/laicode_java/Solution429.java
+++ laicode_java/src/laicode_java/Solution429.java
@@ -7,7 +7,22 @@ import java.util.*;
 //For example, given the array [2,3,1,2,4,3] and s = 7,
 //the subarray [4,3] has the minimal length under the problem constraint.
 public class Solution429 {
-
+	public int minSubArrayLen(int s, int[] nums) {
+		int i=0, j=0, sum=0, min=Integer.MAX_VALUE;
+		while (j<nums.length) {
+			while(sum<s && j<nums.length) {
+				sum +=nums[j++];
+			}
+			if(sum>=s) {
+				while(sum>=s && i<j) {
+					sum-=nums[i++];
+				}
+				min = Math.min(min, j-i+1);
+			}
+		}
+		return min == Integer.MAX_VALUE ? 0 : min;
+	}
+	
 	public static void main(String[] args) {
 
 
